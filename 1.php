ffda253385f35ca7b7a432e91739cdee333c20fa
jkat718 <jkat718@users.noreply.github.com>
2020-07-05 16:04:29 -0400

    Added content
    Content taken from:
    csc174.org/lab01/jchoi84
    csc174.org/lab01/dlai2
    csc174.org/lab01/jkatzner/lab01

--- 1.php
+++ 1.php
@@ -5,9 +5,37 @@ include "inc/html-top.php";
 ?>
 <body>
 <?php include "inc/nav.php"; ?>
-
-
-
+	<main>
+		<article>
+			<section>
+				<p>My name is Joshua Choi and I'm a junior at the University of Rochester double majoring in computer science and digital media studies. I'm from Closter, New Jersey, which isn't known for anything special besides being right next to New York City. I'm a very visually-oriented person and enjoy doing digital art and photography in my free time. I also grew up playing music and can play the clarinet, saxophone, and the cello.</p>
+			</section>
+		</article>
+		
+		<article>
+			<h2 class="title">Digital Art</h2>
+			<section>
+				<p>I have over 6 years of experience with digital art and graphic design and am proficient using Illustrator, Photoshop, and Adobe XD. Check out some of my stuff here: <a class="link" href="https://joshuachoi.herokuapp.com/" target="_blank">My Work</a></p>
+			</section>
+		</article>
+		
+		<article>
+			<h2 class="title">Photography</h2>
+			<section>
+				<p>During my free time I enjoy going out and taking pictures. I shoot on a NikonD5600 and like taking portraits in particular. I like using Photoshop to edit the picture I take. Check out some of my stuff here: <a class="link" href="https://www.instagram.com/choi._.create/" target="_blank">Instagram</a></p>
+			</section>
+		</article>
+		
+		<article>
+			<h2 class="title">SASE</h2>
+			<section>
+				<p>I am the Publicity Chair of the University of Rochester Society of Asian Scientists and Engineers. As the Publicity Chair, I am responsible for all of UR SASE’s advertisements, promotions, etc., both digital and physical, as well as marketing techniques and the promotion of SASE branding. I manage SASE’s social media sites such as Instagram and Facebook and create flyers for different events to put around campus. I also design and create banners for the events that I post on UR SASE’s Facebook page.</p>
+			</section>
+		</article>
+	</main>
+	<footer>
+		Taken from <a href="csc174.org/lab01/jchoi84" target="_blank">csc174.org/lab01/jchoi84</a>
+	</footer>
 <?php include "inc/scripts.php"; ?>
 </body>
 </html>
--- 3.php
+++ 3.php
@@ -5,9 +5,131 @@ include "inc/html-top.php";
 ?>
 <body>
 <?php include "inc/nav.php"; ?>
-
-
-
+	<main>
+		<article>
+			<h2>Jason Katzner</h2>
+				
+			<section>
+				<figure>
+					<img class="flexible" src="images/headshot.jpg" alt="Jason Katzner, wearing a blue shirt and leaning on the table in front of him.">
+					<figcaption>Jason in 2016</figcaption>
+				</figure>
+				<p>Jason Katzner (he/him) is a member of the University of Rochester class of 2021 majoring in Computer Science. He is also in the GRADE program, studying Online Teaching & Learning. He also studies Linguistics and Technical Theater. Outside of his studies, he lives on the Computer Interest Floor, plays tabletop roleplaying games, and LARPs.</p>
+			</section>
+		</article>
+		
+		<article>
+			<h2>Computer Science</h2>
+			
+			<section>
+				<h3>Bachelor's</h3>
+				
+				<figure>
+					<img class="flexible whitefill" src="./images/hajim.png" alt="Unit Mark for Hajim School of Engineering & Applied Sciences at the University of Rochester">
+					<figcaption>Hajim School of Engineering & Applied Sciences</figcaption>
+				</figure>
+				
+				<p>Jason is pursuing a Bachelor of Arts in Computer Science from the Hajim School. Originally on a focus track in Natural Language Understanding, he is considering a last-minute switch to Human-Computer Interaction (HCI). On the professional side of computer science, Jason has worked as an intern at Accrete, a financial technology startup specializing in artificial-intelligence generated market analysis.</p>
+				
+				<aside>
+					<h4>Coursework</h4>
+					
+					<ul>
+						<li>CSC	170: Intro to Web Design & Development</li>
+						<li>CSC	172: Data Structures & Algorithms</li>
+						<li>CSC	173: Computation and Formal Systems</li>
+						<li>CSC	242: Artificial Intelligence</li>
+						<li>CSC	250: Data Science for Linguistics</li>
+						<li>CSC	252: Computer Organization</li>
+						<li>CSC	261: Intro to Databases</li>
+						<li>CSC	280: Computer Models & Limitations</li>
+						<li>CSC	273W: Writing for Computer Science</li>
+						<li>CSC	275: Creative Computing</li>
+						<li>MTH 150: Discrete Mathematics</li>
+						<li>WRT 105: Posthumanism</li>
+						<li>WRT 273: Engineering - Communicating Your Professional Identity</li>
+					</ul>
+				</aside>
+			</section>
+		</article>
+		
+		<article>
+			<h2>Education</h2>
+			
+			<section>
+				<h3>Master's</h3>
+				
+				<figure>
+					<img class="flexible whitefill" src="./images/hajim.png" alt="Unit Mark for Warner School of Education and Human Development at the University of Rochester">
+					<figcaption>Warner School of Education</figcaption>
+				</figure>
+				
+				<p>Jason is pursuing a Master's of Education from the Warner School via the Guaranteed Rochester Accelerated Degree in Education (GRADE) program. He is intending to utilize the combined degree option to also complete an Advanced Certification in Digitally-Rich Teaching in K-12 Schools. Outside of formal education, Jason has worked in afterschool and summer programs, as well as tutoring students in the Java programming language.</p>
+			
+				<aside>
+					<h4>Coursework</h4>
+					
+					<ul class="course-list">
+						<li>EDE	484: Online Teaching & Learning</li>
+						<li>EDE 486: Designing Online Courses</li>
+					</ul>
+				</aside>
+			</section>
+		</article>
+		
+		<article>
+			<section>
+					<h3>Minor: Linguistics</h3>
+					<p>Jason has completed a cluster in Language Structure via the linguistics department. This cluster is defined as one that "focuses on the semantics and pragmatics of natural language and how linguistic forms and their usages give rise to meanings" (<a href="https://secure1.rochester.edu/registrar/CSE/clusterView.php?clusterNumber=S1LIN005" target="_blank">source</a>). In the fall, Jason plans to finish all remaining coursework to turn this cluster into a minor in linguistics.</p>
+				
+					<aside>
+						<h4>Coursework</h4>
+						<ul class="course-list">
+							<li>LIN	110: Introduction to Linguistic Analysis</li>
+							<li>LIN	210: Introduction to Language Sound Systems</li>
+							<li>LIN	220: Introduction to Grammatical Systems</li>
+							<li>LIN	225: Introduction to Semantic Analysis (minor, planned)</li>
+							<li>LIN	250: Data Science for Linguistics (minor)</li>
+						</ul>
+				</aside>
+			</section>
+				
+			<section>
+				<h3>Cluster: Theatre</h3>			
+				<p>Jason has also completed a cluster in Theatre Production and Performance via the theater department (housed in the English department), and then some. He originally intended on completing a certificate in stage management, but realized the extracurricular requirements were too steep to complete alongside a master's degree.</p>
+			
+				<aside>
+					<h4>Coursework</h4>
+					<ul class="course-list">
+						<li>ENG	154: Intro to Design for Stage (cluster)</li>
+						<li>ENG	173: Intro to Stage Lighting and Sound (cluster)</li>
+						<li>ENG	178: Design for Stage: Costumes</li>
+						<li>ENG	178: Design for Stage: Sound (cluster)</li>
+						<li>ENG	179: Design for Stage: Lighting</li>
+						<li>ENG	179: Design for Stage: Scenic</li>
+						<li>ENG	271: Advanced Technical Theater (cluster)</li>
+						<li>ENG	297: Stage Management: Spring 2019 (cluster)</li>
+						<li>ENG	299: Performance Lab: Conduct of Life</li>
+					</ul>
+				</aside>
+			</section>
+			
+			<section>
+				<h3>Electives</h3>
+				<p>Jason occasionally takes some classes that don't count toward his major, grad program, minor, or cluster. These courses are just for fun...and to meet the minimum required 128 credit hours, of course!</p>
+			
+				<aside>
+					<h4>Coursework</h4>
+					<ul class="course-list">
+						<li>EAS	141: Basic Mechanical Fabrication</li>
+						<li>EAS	143: Introduction to Solid Modeling</li>
+						<li>MUR	125: History of Rock Music</li>
+					</ul>
+				</aside>
+			</section>
+		</article>
+	</main>
+	<footer>Taken from <a href="csc174.org/lab01/jkatzner/lab01" target="_blank">csc174.org/lab01/jkatzner/lab01</a></footer>
 <?php include "inc/scripts.php"; ?>
 </body>
 </html>
