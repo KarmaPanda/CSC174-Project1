bae474e50f3a2d7658884663579694592d8b5655
jkat718 <jkat718@users.noreply.github.com>
2020-07-05 17:04:10 -0400

    Added home and nav
    Content for the home page is just the first section of each page.

--- inc/nav.php
+++ inc/nav.php
@@ -1,8 +1,8 @@
 <nav>
     <ul>
-        <li><a href="index.php"></a></li>
-        <li><a href="1.php"></a></li>
-        <li><a href="2.php"></a></li>
-        <li><a href="3.php"></a></li>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="1.php">Joshua Choi</a></li>
+        <li><a href="2.php">Desmond Lai</a></li>
+        <li><a href="3.php">Jason Katzner</a></li>
     </ul>
 </nav>
--- index.php
+++ index.php
@@ -5,9 +5,22 @@ include "inc/html-top.php";
 ?>
 <body>
 <?php include "inc/nav.php"; ?>
-
-
-
+	<main>
+		<article>
+			<h2>Joshua Choi</h2>
+			<p>My name is Joshua Choi and I'm a junior at the University of Rochester double majoring in computer science and digital media studies. I'm from Closter, New Jersey, which isn't known for anything special besides being right next to New York City. I'm a very visually-oriented person and enjoy doing digital art and photography in my free time. I also grew up playing music and can play the clarinet, saxophone, and the cello.</p>
+		</article>
+		
+		<article>
+			<h2>Desmond Lai</h2>
+			<p>I am a rising junior at the University of Rochester majoring in Computer Science (BA). I am from Staten Island, New York, which is one of the five boroughs of New York City. I started my passion for coding since high school where I learned C#, Java, and Python, and continued to learn more during college.</p>
+		</article>
+		
+		<article>
+			<h2>Jason Katzner</h2>
+			<p>Jason Katzner (he/him) is a member of the University of Rochester class of 2021 majoring in Computer Science. He is also in the GRADE program, studying Online Teaching & Learning. He also studies Linguistics and Technical Theater. Outside of his studies, he lives on the Computer Interest Floor, plays tabletop roleplaying games, and LARPs.</p>
+		</article>
+	</main>
 <?php include "inc/scripts.php"; ?>
 </body>
 </html>
